80af7373b24daf42c7b6c5699dd0f822b32c1525
Vaish <vaish.kushagra@gmail.com>
2018-08-04 11:23:02 +0530

    Added Utils class

---
+++ src/com/paypal/utils/SowpodsUtils.java
@@ -0,0 +1,20 @@
+package com.paypal.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SowpodsUtils {
+
+    public static boolean areOneDiffApart(String s1, String s2){
+       return true;
+    }
+
+    public static List<String> getSowPodsList(){
+        List<String> strings = new ArrayList<>();
+        return strings;
+    }
+
+    public static boolean isWordInSowpods(String s){
+        return true;
+    }
+}
