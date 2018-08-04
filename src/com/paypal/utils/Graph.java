2f524b78014a663929d02b091c807675066e9e04
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-04 12:15:36 +0530

    add graph class definitions

--- src/com/paypal/utils/Graph.java
+++ src/com/paypal/utils/Graph.java
@@ -12,11 +12,34 @@ public class Graph {
 
     }
 
-    public void addVertex(String word) {
+    public ArrayList<String> getVertices() {
+        return vertices;
+    }
 
+    public ArrayList<ArrayList<Integer>> getEdges() {
+        return edges;
+    }
+
+    public void addVertex(String word) {
+        if(!vertexExists(word))
+            vertices.add(word);
+    }
+
+    public boolean vertexExists(String word) {
+        for(String s: vertices) {
+            if(s.equals(word))
+                return true;
+        }
+        return false;
     }
 
     public void addEdge(String word1, String word2) {
+
+        if(!vertexExists(word1)) addVertex(word1);
+        if(!vertexExists(word2)) addVertex(word2);
+
+        edges.get(vertices.indexOf(word1)).add(vertices.indexOf(word2));
+        edges.get(vertices.indexOf(word2)).add(vertices.indexOf(word1));
 
     }
 
