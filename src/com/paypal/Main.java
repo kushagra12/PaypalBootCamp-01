29f438a8dd6b5287c7da65cfcea837924a1698d6
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-04 13:55:28 +0530

    fix merge issues

--- src/com/paypal/Main.java
+++ src/com/paypal/Main.java
@@ -1,8 +1,30 @@
 package com.paypal;
 
+import com.paypal.utils.Graph;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+//        Graph g = new Graph();
+//        g.addVertex("A");
+//        g.addVertex("B");
+//        g.addVertex("C");
+//        g.addVertex("D");
+//        g.addVertex("E");
+//
+//        g.addEdge("A", "C");
+//        g.addEdge("C", "E");
+//        g.addEdge("B", "D");
+//
+//        ArrayList<String> path =   g.dfs("A", "D");
+//        for(String node: path) {
+//            System.out.println(node);
+//        }
+
     }
 }
