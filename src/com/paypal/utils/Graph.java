5c898eca0907e7c55e57eb589a7540e27d72ba5c
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-04 11:58:16 +0530

    add graph class declaration

---
+++ src/com/paypal/utils/Graph.java
@@ -0,0 +1,25 @@
+package com.paypal.utils;
+
+import java.util.ArrayList;
+
+public class Graph {
+    ArrayList<String> vertices;
+    ArrayList<ArrayList<Integer>> edges;
+
+    public Graph() {
+        vertices = new ArrayList<String>();
+        edges = new ArrayList<ArrayList<Integer>>();
+
+    }
+
+    public void addVertex(String word) {
+
+    }
+
+    public void addEdge(String word1, String word2) {
+
+    }
+
+
+
+}
